afa8ef285c0aa6a4b56539001bc52c2ca00a64f2
Peter Sims <pas2182@columbia.edu>
2019-03-03 15:48:23 -0500

    Update umap_transform.py

--- umap_transform.py
+++ umap_transform.py
@@ -2,6 +2,13 @@
 import sys
 import numpy as np
 import umap
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] =42
+import seaborn as sns
 
 from scipy.stats.stats import spearmanr
 from scrna_utils import load_marker_matrix
@@ -9,10 +16,11 @@ from scrna_utils import load_marker_matrix
 
 refrun_NAME = sys.argv[1]      # run name for reference dataset (from which original UMAP embedding was defined)
 marker_INFILE = sys.argv[2]    # file containing marker gene list (used to compute original UMAP embedding)
-proj_PREFIX = sys.argv[3]      # prefix for output files containing projection data
-k_PARAM = int(sys.argv[4])     # k parameter for UMAP (number of nearest neighbors for knn graph)
-used_genes = sys.argv[5]       # output file containing list of marker genes actually used (original list may be filtered)
-projrun_NAMES = sys.argv[6::]  # list of run names for projection
+pg_INFILE = sys.argv[3]
+proj_PREFIX = sys.argv[4]      # prefix for output files containing projection data
+k_PARAM = int(sys.argv[5])     # k parameter for UMAP (number of nearest neighbors for knn graph)
+used_genes = sys.argv[6]       # output file containing list of marker genes actually used (original list may be filtered)
+projrun_NAMES = sys.argv[7::]  # list of run names for projection
 
 
 print('Loading data...')
@@ -54,9 +62,29 @@ umap_model = umap.UMAP(n_neighbors=k_PARAM,random_state=42,metric='spearman').fi
 model_OUTFILE = proj_PREFIX+'.umap_proj_model.txt'
 np.savetxt(model_OUTFILE,umap_model.embedding_,delimiter='\t')
 
+pcolor_set = ['red','green','blue','magenta','brown','cyan','black','orange','grey','darkgreen','yellow','tan','seagreen','fuchsia','gold','olive']
+pgs = np.loadtxt(pg_INFILE,dtype='int')
+pcolors = np.array([pcolor_set[pg] for pg in pgs])
 print('Computing projections...')
 for i in range(len(pdata_filt)):
 	umap_proj = umap_model.transform(np.float32(pdata_filt[i].T))
 	proj_OUTFILE = proj_PREFIX+'.'+projrun_NAMES[i]+'.umap_proj.txt'
+	pdf_OUTFILE = proj_PREFIX+'.'+projrun_NAMES[i]+'.umap_proj.pdf'
 	np.savetxt(proj_OUTFILE,umap_proj,delimiter='\t')
-	
+	with PdfPages(pdf_OUTFILE) as pdf:
+		fig=plt.figure(figsize=(10,5))
+		rind = np.argsort(np.random.rand(len(msamples)))  # random index 
+
+		ax1 = fig.add_subplot(1,2,1)
+		ax1.scatter(umap_model[rind,0],umap_model[rind,1],c=pcolors[rind],s=1)
+		sns.kdeplot(umap_proj[:,0],umap_proj[:,1],cmap='binary_r',shade=False,gridsize=70,n_levels=14)
+		ax1.set_aspect('equal')
+		ax1.set_axis_off()
+
+		ax2 = fig.add_subplot(1,2,2)
+		ax2.scatter(umap_model[rind,0],umap_model[rind,1],c='lightgrey',s=1)
+		x=ax2.hexbin(umap_proj[:,0],umap_proj[:,1],cmap='plasma',mincnt=2,alpha=0.8,linewidths=0,edgecolors=None,gridsize=70)
+		plt.colorbar(x,shrink=0.6)
+		ax2.set_aspect('equal')
+		ax2.set_axis_off()
+		pdf.savefig()
